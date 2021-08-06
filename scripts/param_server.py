9e3d8d7ca9781d3c5d138fc120129cc33840765e
Cristian Chitiva <cychitivav@unal.edu.co>
2021-08-06 12:07:32 -0500

    First tune PID gains

--- scripts/param_server.py
+++ scripts/param_server.py
@@ -4,15 +4,16 @@ import rospy
 from dynamic_reconfigure.server import Server
 from kobuki_control.cfg import PIDConfig
 
+
 def callback(config, level):
-    rospy.loginfo("""Linear velocity:\n Kp={Kpv}, Ki={Kiv}, Kd={Kdv}""".format(**config))
-    rospy.loginfo("""Angular velocity:\n Kp={Kpw}, Ki={Kiw}, Kd={Kdw}""".format(**config))
-    
+    rospy.loginfo("""\nLinear velocity:\n Kp={Kpv}, Ki={Kiv}, Kd={Kdv}""".format(
+        **config) + """\nAngular velocity:\n Kp={Kpw}, Ki={Kiw}, Kd={Kdw}""".format(**config))
+
     return config
+
 
 if __name__ == "__main__":
     rospy.init_node("param_server", anonymous=True)
-    
+
     srv = Server(PIDConfig, callback)
     rospy.spin()
-
